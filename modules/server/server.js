eb445951b429e337ff24881537ddaf6857e8edb7
Emmanuek5 <emmanuelandrew625@yahoo.com>
2022-12-13 21:56:07 -0500

    Work in Progress 5

---
+++ modules/server/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+app.listen(3000,()=>{
+  pridnt = "Application start"
+})
+
+
+
+
+function start(params) {
+    
+console.log(pridnt)
+
+}
