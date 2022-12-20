56c2c19e8b368e4a95734b228da10817938ba9c6
Emmanuek5 <emmanuelandrew625@yahoo.com>
2022-12-19 22:32:36 -0500

    Major Progress Yess

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const qrcode = require('qrcode-terminal');
 const mail = require("./modules/mail.js")
 const server = require("./modules/server/server")
 const db = require("./modules/database/config")
-
+const log = require('.//modules/server/logger')
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
@@ -23,36 +23,32 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    const path = './logs/' + msg.id + '.txt'
-
-    if (!fs.existsSync(path)) {
-        fs.writeFileSync(path, date.toLocaleString + " : " + msg.body);
-        console.log(`File ${fileName} created.`);
-    } else {
-        fs.writeFileSync(path, msg.body);
-    }
-    console.log("New Message  From "+msg.from+" Body: " + msg.body);
     
-    if(msg.body == "1") {
+    console.log("New Message  From " + msg.from + " Body: " + msg.body);
+  log.log(msg.body,msg.from)
+    if (msg.body == "1") {
         msg.reply("Select The Option That Suits Your Needs: \n Agro 1) Buy Meat/Chicken \n Agro 2) Buy Household Items \n Agro 3) Foodstuff \n Agro 4) Special Order  \n Note: Special Orders Have A N500 Fee ")
-    }
-
-    if (msg.body == "Agro 1") {
-
-        msg.reply("Select The Size: \n M 1) Meat 1kg  \n M 2) Meat 5kg \n C 3) Chicken 1kg \n C 4) Chicken 5kg")
-
-
-    }
-    if (msg.body = "M 1") {
-        msg.reply("Thank You For You Order. The Closest Dispatch Rider Will Contact You With The Details Of Your Order. ")
-   mail.sendmail("andrewgbemisola@gmail.com","New Order HAS Been Placed For Meat 1kg At Price n1500 ","New Order","fsgsolutions@gmail.com")
     }else{
+        if (msg.body == "Agro 1") {
 
-    client.sendMessage(msg.from, " Hello This Is FSG WORK SOLUTIONS \n  Select THe Options : \n 1) Fsg Agro \n 2) FSG Employments & Oppurtunities   \n 3) Fsg Investment Programs")
+            msg.reply("Select The Size: \n M1) Meat 1kg  \n M2) Meat 5kg \n C3) Chicken 1kg \n C4) Chicken 5kg")
 
-    
 
-}}
+        } else {
+            if (msg.body == "M1") {
+                msg.reply("Thank You For You Order. The Closest Dispatch Rider Will Contact You With The Details Of Your Order. ")
+                mail.sendmail("rccgheritageofgod@gmail.com", "New Order HAS Been Placed For Meat 1kg At Price N1500 ", "New Order", "fsgsolutions@gmail.com")
+            } else {
+
+                client.sendMessage(msg.from, " Hello This Is FSG WORK SOLUTIONS \n  Select THe Options : \n 1) Fsg Agro \n 2) FSG Employments & Oppurtunities   \n 3) Fsg Investment Programs")
+            }
+}
+        
+    }
+
+
+   
+}
 
  
 
